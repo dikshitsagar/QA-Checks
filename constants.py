022c9e411ec5665f45eb96a2f0a62ed250b40fc4
Dikshit Sagar <dikshitsagar@gmail.com>
2023-03-20 20:41:10 +0530

    Add files via upload

--- constants.py
+++ constants.py
@@ -3,7 +3,9 @@ import pandas as pd
 states_df = pd.read_csv("states.csv") 
 C_default_val = 'IONCUST'
 C_default_val_col = 'CUTM'
+C_col_dict_val_default = {'CUTM':'IONCUST','LNCD':'GB','ECAR':'IA'}
 ##############################
+C_col_dict_val_len = {'CUNO':5,'LNCD':2,'CUTM':7}
 C_col_val_len = 5
 C_val_len_col = 'CUNO'
 ##############################
